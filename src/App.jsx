c2297642bced9c502741b6ef06a95bbe320c930d
Himanshukumar786 <himanshu.kumar0104@gmail.com>
2024-09-01 17:29:16 +0530

    Added Input Text

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-
-import './App.css'
+import TextInputForm from "./components/TextInputForm/TextInputForm";
 
 function App() {
-  return(
-    <>
-      <h1>
-        Hy !!!,
-      </h1>
-    </>
-  )
+  return (
+    <div>
+      <h1 className="font-semibold text-3xl">Welcome to Hang-Man</h1>
+      <TextInputForm onSubmit={(value) => console.log("value from the hidden form is", value)}/>
+    </div>
+  );
 }
 
-export default App
+export default App;
